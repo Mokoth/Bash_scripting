7ddcb62722d49a00393403f26d3a0856d20298b1
Mokoth <onyach.okoth1@outlook.com>
2022-09-24 04:55:10 +0000

    Recap on some commands

---
+++ bash-variables/cademy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+echo "Commands for creating and traversing folders:"
+echo "mkdir test"
+echo "cd test test1"
+echo "ls"
+echo "Commands for creating files and checking contents:-"
+echo "touch file file1 file2"
+echo "ls"
+echo "echo 'Hello, Mom' > file"
+echo "echo 'Your Son >> file'"
+echo "cat file"
+
